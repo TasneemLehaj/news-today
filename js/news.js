574583f8a53e09f31166b02a4775c33009ae733d
TasneemLehaj <tasneemlehaj20@gmail.com>
2022-09-03 22:42:35 +0600

    half work done

--- js/news.js
+++ js/news.js
@@ -1,51 +1,147 @@
-const loadNewsCatagories = () => {
-    const url = ` https://openapi.programming-hero.com/api/news/categories`;
-    fetch(url)
-        .then(res => res.json())
-        .then(data => displayCatagories(data.data.news_category))
+// call category api
+const loadCategory = async () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    try {
+        const response = await fetch(url)
+        const data = await response.json();
+        return data.data.news_category;
+    }
+    catch (error) {
+        console.log(error)
+    }
 
 }
-
-const displayCatagories = (newsCategories) => {
-    const catagoryContainer = document.getElementById('catagory-container');
-    newsCategories.forEach(news => {
-        // console.log(news);
-        const catagoryDiv = document.createElement('div');
-        catagoryDiv.classList.add('col');
-        catagoryDiv.innerHTML = `
-        <button id="catagory-news" onclick="loadNews()" class="fs-6 d-grid gap-2 border-0" > ${news.category_name}</button>
-        `;
-        catagoryContainer.appendChild(catagoryDiv);
-
+//display category
+const categoryDisplay = async () => {
+    const data = await loadCategory();
+    data.forEach(category => {
+        let categorySection = document.getElementById('category-area');
+        const createCategory = document.createElement('div')
+        createCategory.innerHTML = `
+        <div class="d-grid gap-2 mb-1">
+            <button onClick="loadNews(${category.category_id})" class="btn btn-outline-success" type="button">${category.category_name}</button>
+        </div>
+        `
+        categorySection.appendChild(createCategory);
     })
 }
-const url = ` https://openapi.programming-hero.com/api/news/category/01`;
-const loadNews = () => {
-    fetch(url)
-        .then(res => res.json())
-        .then(data => console.log(data))
+// load data after click category button
+
+// call news api 
+const loadNews = async (category_id) => {
+    // Spinner added and start 
+    toggleSpinner(true);
+
+    const response = await fetch(`https://openapi.programming-hero.com/api/news/category/0${category_id}`)
+    const newses = await response.json();
+    const newsesData = newses.data;
+
+    const newsFound = document.getElementById('news-found');
+    newsFound.innerHTML = ''
+    const newsFoundText = document.createElement('div');
+    if (newsesData.length > 0) {
+        newsFoundText.innerHTML = `<h5 id="news-found" class="text-center border border-dark p-1 mb-2">News Found: ${newsesData.length}</h5>`;
+        newsFound.appendChild(newsFoundText);
+    } else {
+        newsFoundText.innerHTML = `<h5 id="news-found" class="text-center border border-dark p-1">Sorry No News Found</h5>`;
+        newsFound.appendChild(newsFoundText);
+    }
+    return displayNews(newsesData);
+}
+
+
+// display news after clicking a category
+const displayNews = async (newsesData) => {
+
+    // sort array by total_view
+    let x = newsesData.sort((a, b) => (b.total_view > a.total_view ? 1 : -1));
+
+    const newsSection = document.getElementById('news-area');
+    newsSection.innerHTML = "";
+
+    newsesData.forEach(news => {
+        const {
+            title,
+            _id,
+            details,
+            image_url,
+            author,
+            total_view
+        } = news;
+
+        const createSingleNews = document.createElement('div');
+        createSingleNews.innerHTML = `
+        <div class="card mb-3">
+            <div class="row g-0 align-items-center">
+                <div class="col-md-4">
+                    <img src="${image_url}" class="img-fluid rounded-start" alt="...">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title text-center text-md-start">${title}</h5>
+                        <p class="card-text">${details.slice(0, 300)} [...]</p>
+                        <div class="m-1 border p-2 align-items-center row">
+                            <div class="col-9 col-md-6 d-flex align-items-center">
+                                <img src="${author.img}" class="img-fluid news-author-img rounded-circle"/>
+                                <p class="ms-3">Name: ${author.name ? news.author.name : 'Name not found'} <br />
+                                    
+                                </p>
+                            </div>
+                            <div class="col-3 col-md-3">
+                                <span><i class="fa-regular fa-eye"></i> ${total_view ? total_view : '00'}</span>
+                            </div>
+                            <div class="col-12 col-md-3">
+                                <button type="button" class="btn btn-outline-dark w-100" data-bs-toggle="modal" data-bs-target="#exampleModal" onClick='newsDetailsLoad("${_id}")'>Details<i class="fa-solid fa-play ms-1"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `
+        newsSection.appendChild(createSingleNews);
+
+    })
+
+    toggleSpinner(false)
 
 }
-// const loadNewsContainer = document.getElementById('loadNews-container');
-// const newsInACatagory = document.createElement('div');
-// newsInACatagory.innerHTML = `
-// <div class="container card mb-3">
-// <div class="row g-0">
-//     <div class="col-md-4">
-//         <img src="..." class="img-fluid rounded-start" alt="...">
+
+// Spinner 
+const toggleSpinner = isLoading => {
+    const spinner = document.getElementById('spinner')
+    if (isLoading) {
+        spinner.classList.remove('d-none');
+    } else {
+        spinner.classList.add('d-none');
+    }
+}
+
+// load newsDetails api
+const newsDetailsLoad = async (id) => {
+    const response = await fetch(`https://openapi.programming-hero.com/api/news/${id}`)
+    const newsData = await response.json();
+    displayNewsDetails(newsData.data)
+}
+
+// dispaly news details useing a modal
+// const displayNewsDetails = (newsData) => {
+//     const modalContainer = document.getElementById('news-detais-modal');
+//     modalContainer.innerHTML = ""
+//     const modalDiv = document.createElement('div');
+//     modalDiv.innerHTML = `
+//     <div class="modal-header bg-success text-white text-justify">
+//         <p class="modal-title" id="exampleModalLabel">${newsData[0].title}</p>
+//         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
 //     </div>
-//     <div class="col-md-8">
-//         <div class="card-body">
-//             <h5 class="card-title">Card title</h5>
-//             <p class="card-text">This is a wider card with supporting text below as a natural
-//                 lead-in to additional content. This content is a little bit longer.</p>
-//             <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-//         </div>
+//     <div class="modal-body">
+//         <span>${newsData[0].details}</span>
 //     </div>
-// </div>
-// </div>
-// `;
+//     <div class="modal-footer">
+//         <button type="button" class="btn btn-outline-success" data-bs-dismiss="modal">Close</button>
+//     </div>
+//     `
+//     modalContainer.appendChild(modalDiv)
+// }
 
-
-
-loadNewsCatagories();
+categoryDisplay()
